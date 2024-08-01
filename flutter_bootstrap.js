3055090b3f08293f4506b8798e949f60192be2c9
sunyongIM <imsunyong@naver.com>
2024-08-02 02:19:03 +0900

    디자인 테스트01

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "211138880"
+    serviceWorkerVersion: "2026802498"
   }
 });
